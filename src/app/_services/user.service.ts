d79c5f6802c5bed48ba7cc392096144f98ca9083
Doragonware <simondk123@gmail.com>
2019-02-17 09:56:26 +0100

    Update members implemented

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -10,7 +10,7 @@ import { User } from '../_models/user';
 export class UserService {
   baseUrl = environment.apiUrl;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getUsers(): Observable<User[]> {
     return this.http.get<User[]>(this.baseUrl + 'users');
@@ -18,5 +18,9 @@ export class UserService {
 
   getUser(id): Observable<User> {
     return this.http.get<User>(this.baseUrl + 'users/' + id);
+  }
+
+  updateUser(id: number, user: User) {
+    return this.http.put(this.baseUrl + 'users/' + id, user);
   }
 }
